3f6e87549e908ec5e0a6aacdd8762294ecadb4ba
Jorge Aguiar Martín <alu0100823295@ull.edu.es>
2018-04-21 16:56:14 +0100

    Hecho los cambios necesarios para que las tarjetas sean accesibles

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,18 +1,25 @@
-$(document).ready(function(){
-    $('.parallax').parallax();
-});
-
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-});
-
 
 
 // ToDo: Terminar de solucionar esto
-$('.activator').on('keypress' , function() {
-    console.log("HEY");
-    this.click();
-    let cardReveal = this.closest('.card-reveal');
+$('.activator').on('keypress' , function( event ) {
+
+    var target = event.target;
+    target.click();
+    var card = target.closest('.card');
+    var cardReveal = card.querySelector('.card-reveal');
+    var cardTitle = cardReveal.querySelector('.card-title');
+    cardTitle.focus();
 
 });
 
+
+$('.card-reveal').find('.card-title').on('keypress', function ( event ) {
+
+    var target = event.target;
+    target.click();
+    var card = target.closest('.card');
+    var cardContent = card.querySelector('.card-content');
+    var cardTitle = cardContent.querySelector('.card-title');
+    cardTitle.focus();
+
+});
